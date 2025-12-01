6e58c2ac66ae35544636ad8ddac9fe09ddc67fbc
Pedro Moraes <pindamonhangaba@users.noreply.github.com>
2025-12-01 09:13:38 -0300

    Update mapstructure import to v2 version

--- custom.go
+++ custom.go
@@ -4,7 +4,7 @@ import (
 	"reflect"
 	"strings"
 
-	"github.com/mitchellh/mapstructure"
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
